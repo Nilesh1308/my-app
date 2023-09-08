a3fc20c089b0489c2d469c67dd1d8d054d5b884c
Nilesh1308 <nileshmulchandani1308@gmail.com>
2023-09-08 18:41:45 +0530

    react learning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 function App() {
+  const [mode, setMode] = useState('dark'); //wheather dark mode is enabled or not
+
+  const toggleMode = ()=> {
+      if(mode === 'light'){
+      setMode('dark')
+      }
+      else{
+      setMode ('light')
+      }
+  }
+ 
   return (
    <>
 
-<Navbar title="TextUtils" AboutText="About TextUtils"/>
+<Navbar title="TextUtils" AboutText="About TextUtils" mode={mode} toggleMode = {toggleMode}/>
 <div className="container my-3">
-<Textform heading="Enter The Text to analyze below"/>
+{<Textform heading="Enter The Text to analyze below"/>}
+{/*<About/>*/}
 </div>
    </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -15,6 +15,11 @@ export default function Textform(props) {
     let newText= '';
   setText(newText);
 }
+const handleCopy = ()=>{
+  let text= document.getElementById("myBox");
+       text.select ();
+       navigator.clipboard.writeText(text.value)
+}
   const handleOnChange = (event)=>{
     setText(event.target.value);
   }
@@ -29,6 +34,7 @@ export default function Textform(props) {
             <button className="btn btn-primary mx-1"onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1"onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-1"onClick={handleCClick}>Clear Text</button>
+            <button className="btn btn-primary mx-1"onClick={handleCopy}>Copy Text</button>
     </div>
     <div className="container my-3">
       <h2>Your Text Summary</h2>
